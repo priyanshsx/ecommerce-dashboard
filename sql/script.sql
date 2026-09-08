588bdfb0e2cad64803eeed67161030537c31ee78
Priyansh <saxenapriyansh95@gmail.com>
2026-09-08 14:02:54 +0530

    understood the database schema for further analysis

--- sql/script.sql
+++ sql/script.sql
@@ -9,3 +9,18 @@ SELECT * FROM table_name
 -- checking for NULLs 
 
 SELECT * FROM table_name WHERE col1 IS NULL OR col2 IS NULL...
+
+-- understanding the schema: 
+-- order_id is common between: 
+-- order_payments_dataset, orders_dataset, order_reviews_dataset, and order_items_dataset
+
+-- zip_code_prefix is common between: 
+-- customers_dataset and geolocation_dataset
+-- how are customer_zip_code_prefix and geolocation_zip_code_prefix different to each other? 
+
+-- orders_dataset and customers_dataset can be combined ON 
+-- customer_id 
+
+-- order_items_dataset and products_dataset can be combined on 
+-- seller_id
+ 
