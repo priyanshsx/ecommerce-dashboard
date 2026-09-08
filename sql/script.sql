fe1c4bc600e733d41c9506df57c09442611b4a0b
Priyansh <saxenapriyansh95@gmail.com>
2026-09-08 12:56:27 +0530

    added the null script for sql

--- sql/script.sql
+++ sql/script.sql
@@ -5,3 +5,7 @@ CREATE TABLE table_name AS SELECT * FROM read_csv_auto('csv_file_name.csv')
 -- sanity check 
 
 SELECT * FROM table_name 
+
+-- checking for NULLs 
+
+SELECT * FROM table_name WHERE col1 IS NULL OR col2 IS NULL...
