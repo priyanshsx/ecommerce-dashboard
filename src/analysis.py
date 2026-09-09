5e4b0b7c180f00b44f87633fa8558d1bc638dcc6
Priyansh <saxenapriyansh95@gmail.com>
2026-09-09 15:56:34 +0530

    added the chart for question 1

--- src/analysis.py
+++ src/analysis.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np 
 import duckdb 
 
+# duckdb for importing tables 
 con = duckdb.connect('/home/priyansh/Documents/d/ecommerce dashboard/db/main.duckdb')
 
 df = con.sql("""
@@ -10,21 +11,31 @@ df = con.sql("""
     FROM question_one
 """).df()
 
-# 3. Aggregate the data for plotting
-# (Summing revenue by category and grabbing the Top 10)
-top_categories = df.groupby('product_category_name_english')['total_revenue'].sum().nlargest(10)
+# finding the top 10 cities and top 10 categories overall without the exact numbers  
+top_cities = df.groupby('customer_city')['total_revenue'].sum().nlargest(10).index
+top_categories = df.groupby('product_category_name_english')['total_revenue'].sum().nlargest(10).index 
 
-# 4. Create the visualization
-plt.figure(figsize=(12, 6))
-top_categories.plot(kind='bar', color='steelblue')
+# create a new df to only include the top 10 performers 
+filtered_df = df[df['customer_city'].isin(top_cities) & df['product_category_name_english'].isin(top_categories)]
 
-# 5. Format the chart to make it presentation-ready
-plt.title('Top 10 Product Categories by Revenue', fontsize=16)
-plt.xlabel('Product Category', fontsize=12)
+# created a pivot table to order data into a wide format from a long format for the stacked bar chart 
+pivot_df = filtered_df.pivot_table(
+    index='customer_city',
+    columns='product_category_name_english',
+    values='total_revenue',
+    aggfunc='sum'
+)
+
+# plotting the chart 
+pivot_df.plot(kind='bar', stacked=True, figsize=(12, 7), colormap='viridis')
+
+plt.title('Top 10 Product Categories Revenue across Top 10 Cities', fontsize=16)
+plt.xlabel('Customer City', fontsize=12)
 plt.ylabel('Total Revenue (BRL)', fontsize=12)
-plt.xticks(rotation=45, ha='right') # Tilts the labels so they don't overlap
-plt.tight_layout() # Ensures nothing gets cut off
+plt.xticks(rotation=45, ha='right')
+plt.legend(title='Product Category', bbox_to_anchor=(1.05,1), loc='upper left')
+plt.tight_layout()
 
-# Show the plot!
-plt.savefig('top_10_categories.png', dpi=300)
-print("Chart saved as top_10_categories.png")
+plt.savefig('/home/priyansh/Documents/d/ecommerce dashboard/figures/combined_revenue_geography.png', dpi=300)
+print('Image saved to the given folder.')
+
